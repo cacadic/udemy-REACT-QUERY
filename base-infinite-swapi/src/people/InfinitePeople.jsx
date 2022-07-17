cb3397c6282c5f480589d34bb6b33f8899cb8cf7
Linh Pham <phamviethonglinh@gmail.com>
2022-07-17 17:45:51 +0700

    24

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,26 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data?.pages?.map((pageData) =>
+        pageData.results.map((person, index) => (
+          <Person
+            key={index}
+            name={person.name}
+            hairColor={person.hair_color}
+            eyeColor={person.eye_color}
+          />
+        ))
+      )}
+    </InfiniteScroll>
+  );
 }
